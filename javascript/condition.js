b763402f419f0e73b804a6feb8b6934cd5cb8c5b
emytech <azstudio04@gmail.com>
2022-03-27 04:26:09 -0700

    condition

---
+++ javascript/condition.js
@@ -0,0 +1,17 @@
+let customerWallet = 1000000;
+let canonM50Price = 200000;
+
+if(customerWallet >= canonM50Price){
+    alert("camera bought, will send it to you soon")
+}else{
+    alert("Oga, you have "+ customerWallet + " in your wallet and you want to purchase camera of "+ canonM50Price+ ". Do you think we are mumu?")
+}
+
+let emmaWallet = 200;
+let canonEOS400D =290000;
+if(emmaWallet >= canonEOS400D){
+    console.log("successfully");
+}else{
+    console.log("oga you be theif");
+}
+
